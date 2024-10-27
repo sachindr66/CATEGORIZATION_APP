7b799066945419d74cf29c13208a5e7d28a95b46
sachindr66 <sachindrhsn@gmail.com>
2024-10-27 21:31:51 +0530

    second coommit

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,5 +1,7 @@
+// Category
 const express = require('express');
 const { getCategories } = require('../controllers/categoryController');
+const Category = require('../models/Category');
 const router = express.Router();
 
 router.get('/', getCategories);
