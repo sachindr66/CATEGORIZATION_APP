abdcc75a241b3c26b26200e5a5f94aa18e820387
sachindr66 <sachindrhsn@gmail.com>
2024-10-28 11:45:49 +0530

    second coommit

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/user', userRoutes); 
 
+app.use('*', (req, res) => {
+    res.status(404).json({ message: "Endpoint not found" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
